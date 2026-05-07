fac94cefac1b877c3d9e30970b43d7a468c09fc1
Marco <marco.mangan@gmail.com>
2026-05-07 14:00:18 +0000

    chore: Adding delete

--- trc.c
+++ trc.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
+int contains(char *s, char c)
+{
+    while (*s)
+    {
+        if (*s == c)
+        {
+            return 1;
+        }
+        s++;
+    }
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
     if (argc == 1)
     {
-        printf("trc: missing operand\n");
+        printf("trc: missing operand(1)\n");
         printf("Try 'trc --help' for more information.\n");
     }
     else if (argc == 2)
@@ -21,9 +34,42 @@ int main(int argc, char **argv)
             printf("Interpreted sequences are:\n");
 
             printf("    \n              new line\n");
-        } else {
-            printf("invalid option -- '%c'\n", argv[1][1]);
+        }
+        else
+        {
+            printf("invalid option '%s'\n(2)", argv[1]);
+            printf("Try 'trc --help' for more information.\n");
         }
     }
+    else if (argc == 3)
+    {
+        if (!strcmp("--delete", argv[1]))
+        {
+            // tr --delete '\n'
+            // printf("%s\n", argv[2]);
+
+            int c;
+
+            while ((c = getchar()) != EOF)
+            {
+                if (!contains(argv[2], c))
+                {
+                    putchar(c);
+                }
+            }
+            //putchar('\n');
+        }
+        else
+        {
+            printf("invalid option '%s'\n(3)", argv[1]);
+            printf("Try 'trc --help' for more information.\n");
+        }
+    }
+    else
+    {
+        printf("trc: missing operand(4)\n");
+        printf("Try 'trc --help' for more information.\n");
+    }
+
     return 0;
 }
